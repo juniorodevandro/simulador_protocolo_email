cb7670686424117a4f57ec51bad45b0895e874d5
juniorodevandro <evandrorodejunior@gmail.com>
2022-03-21 22:17:07 -0300

    commit

---
+++ ProtocoloEmail/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace ProtocoloEmail
+{
+    partial class email
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(email));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBoxEnviarEmail = new System.Windows.Forms.CheckBox();
+            this.TextBoxEmailDest = new System.Windows.Forms.TextBox();
+            this.TextBoxSenha = new System.Windows.Forms.TextBox();
+            this.LabelEmailDest = new System.Windows.Forms.Label();
+            this.BotaoEnviar = new System.Windows.Forms.Button();
+            this.LabelMensagem = new System.Windows.Forms.Label();
+            this.LabelSenha = new System.Windows.Forms.Label();
+            this.TextBoxMensagem = new System.Windows.Forms.TextBox();
+            this.TextBoxAssunto = new System.Windows.Forms.TextBox();
+            this.LabelAssunto = new System.Windows.Forms.Label();
+            this.TextBoxEmailRem = new System.Windows.Forms.TextBox();
+            this.LabelEmailRem = new System.Windows.Forms.Label();
+            this.Remetente = new System.Windows.Forms.Label();
+            this.TextBoxRequisicoes = new System.Windows.Forms.TextBox();
+            this.LabelRequisicoes = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.groupBox1.Controls.Add(this.checkBoxEnviarEmail);
+            this.groupBox1.Controls.Add(this.TextBoxEmailDest);
+            this.groupBox1.Controls.Add(this.TextBoxSenha);
+            this.groupBox1.Controls.Add(this.LabelEmailDest);
+            this.groupBox1.Controls.Add(this.BotaoEnviar);
+            this.groupBox1.Controls.Add(this.LabelMensagem);
+            this.groupBox1.Controls.Add(this.LabelSenha);
+            this.groupBox1.Controls.Add(this.TextBoxMensagem);
+            this.groupBox1.Controls.Add(this.TextBoxAssunto);
+            this.groupBox1.Controls.Add(this.LabelAssunto);
+            this.groupBox1.Controls.Add(this.TextBoxEmailRem);
+            this.groupBox1.Controls.Add(this.LabelEmailRem);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(28, 76);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(0);
+            this.groupBox1.Size = new System.Drawing.Size(397, 567);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // checkBoxEnviarEmail
+            // 
+            this.checkBoxEnviarEmail.AutoSize = true;
+            this.checkBoxEnviarEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxEnviarEmail.ForeColor = System.Drawing.Color.White;
+            this.checkBoxEnviarEmail.Location = new System.Drawing.Point(29, 254);
+            this.checkBoxEnviarEmail.Name = "checkBoxEnviarEmail";
+            this.checkBoxEnviarEmail.Size = new System.Drawing.Size(337, 24);
+            this.checkBoxEnviarEmail.TabIndex = 12;
+            this.checkBoxEnviarEmail.Text = "Enviar o e-mail de forma real ao destinatário";
+            this.checkBoxEnviarEmail.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxEmailDest
+            // 
+            this.TextBoxEmailDest.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
+            this.TextBoxEmailDest.ForeColor = System.Drawing.Color.White;
+            this.TextBoxEmailDest.Location = new System.Drawing.Point(9, 160);
+            this.TextBoxEmailDest.Name = "TextBoxEmailDest";
+            this.TextBoxEmailDest.Size = new System.Drawing.Size(385, 20);
+            this.TextBoxEmailDest.TabIndex = 11;
+            // 
+            // TextBoxSenha
+            // 
+            this.TextBoxSenha.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
+            this.TextBoxSenha.ForeColor = System.Drawing.Color.White;
+            this.TextBoxSenha.Location = new System.Drawing.Point(6, 105);
+            this.TextBoxSenha.Name = "TextBoxSenha";
+            this.TextBoxSenha.Size = new System.Drawing.Size(385, 20);
+            this.TextBoxSenha.TabIndex = 11;
+            this.TextBoxSenha.UseSystemPasswordChar = true;
+            // 
+            // LabelEmailDest
+            // 
+            this.LabelEmailDest.AutoSize = true;
+            this.LabelEmailDest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelEmailDest.ForeColor = System.Drawing.Color.White;
+            this.LabelEmailDest.Location = new System.Drawing.Point(127, 137);
+            this.LabelEmailDest.Name = "LabelEmailDest";
+            this.LabelEmailDest.Size = new System.Drawing.Size(140, 20);
+            this.LabelEmailDest.TabIndex = 10;
+            this.LabelEmailDest.Text = "E-mail destinatário";
+            // 
+            // BotaoEnviar
+            // 
+            this.BotaoEnviar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
+            this.BotaoEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotaoEnviar.ForeColor = System.Drawing.Color.White;
+            this.BotaoEnviar.Location = new System.Drawing.Point(128, 511);
+            this.BotaoEnviar.Name = "BotaoEnviar";
+            this.BotaoEnviar.Size = new System.Drawing.Size(135, 41);
+            this.BotaoEnviar.TabIndex = 9;
+            this.BotaoEnviar.Text = "Enviar";
+            this.BotaoEnviar.UseVisualStyleBackColor = false;
+            this.BotaoEnviar.Click += new System.EventHandler(this.BotaoEnviar_Click);
+            // 
+            // LabelMensagem
+            // 
+            this.LabelMensagem.AutoSize = true;
+            this.LabelMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelMensagem.ForeColor = System.Drawing.Color.White;
+            this.LabelMensagem.Location = new System.Drawing.Point(148, 290);
+            this.LabelMensagem.Name = "LabelMensagem";
+            this.LabelMensagem.Size = new System.Drawing.Size(88, 20);
+            this.LabelMensagem.TabIndex = 8;
+            this.LabelMensagem.Text = "Mensagem";
+            // 
+            // LabelSenha
+            // 
+            this.LabelSenha.AutoSize = true;
+            this.LabelSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelSenha.ForeColor = System.Drawing.Color.White;
+            this.LabelSenha.Location = new System.Drawing.Point(99, 80);
+            this.LabelSenha.Name = "LabelSenha";
+            this.LabelSenha.Size = new System.Drawing.Size(201, 20);
+            this.LabelSenha.TabIndex = 10;
+            this.LabelSenha.Text = "Senha do e-mail remetente";
+            // 
+            // TextBoxMensagem
+            // 
+            this.TextBoxMensagem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
+            this.TextBoxMensagem.ForeColor = System.Drawing.Color.White;
+            this.TextBoxMensagem.Location = new System.Drawing.Point(6, 313);
+            this.TextBoxMensagem.Multiline = true;
+            this.TextBoxMensagem.Name = "TextBoxMensagem";
+            this.TextBoxMensagem.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBoxMensagem.Size = new System.Drawing.Size(385, 189);
+            this.TextBoxMensagem.TabIndex = 7;
+            // 
+            // TextBoxAssunto
+            // 
+            this.TextBoxAssunto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
+            this.TextBoxAssunto.ForeColor = System.Drawing.Color.White;
+            this.TextBoxAssunto.Location = new System.Drawing.Point(6, 215);
+            this.TextBoxAssunto.Name = "TextBoxAssunto";
+            this.TextBoxAssunto.Size = new System.Drawing.Size(385, 20);
+            this.TextBoxAssunto.TabIndex = 3;
+            // 
+            // LabelAssunto
+            // 
+            this.LabelAssunto.AutoSize = true;
+            this.LabelAssunto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelAssunto.ForeColor = System.Drawing.Color.White;
+            this.LabelAssunto.Location = new System.Drawing.Point(159, 192);
+            this.LabelAssunto.Name = "LabelAssunto";
+            this.LabelAssunto.Size = new System.Drawing.Size(68, 20);
+            this.LabelAssunto.TabIndex = 2;
+            this.LabelAssunto.Text = "Assunto";
+            // 
+            // TextBoxEmailRem
+            // 
+            this.TextBoxEmailRem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
+            this.TextBoxEmailRem.ForeColor = System.Drawing.Color.White;
+            this.TextBoxEmailRem.Location = new System.Drawing.Point(6, 52);
+            this.TextBoxEmailRem.Name = "TextBoxEmailRem";
+            this.TextBoxEmailRem.Size = new System.Drawing.Size(385, 20);
+            this.TextBoxEmailRem.TabIndex = 1;
+            // 
+            // LabelEmailRem
+            // 
+            this.LabelEmailRem.AutoSize = true;
+            this.LabelEmailRem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelEmailRem.ForeColor = System.Drawing.Color.White;
+            this.LabelEmailRem.Location = new System.Drawing.Point(129, 27);
+            this.LabelEmailRem.Name = "LabelEmailRem";
+            this.LabelEmailRem.Size = new System.Drawing.Size(130, 20);
+            this.LabelEmailRem.TabIndex = 0;
+            this.LabelEmailRem.Text = "E-mail remetente";
+            // 
+            // Remetente
+            // 
+            this.Remetente.AutoSize = true;
+            this.Remetente.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Remetente.ForeColor = System.Drawing.Color.White;
+            this.Remetente.Location = new System.Drawing.Point(122, 43);
+            this.Remetente.Name = "Remetente";
+            this.Remetente.Size = new System.Drawing.Size(195, 29);
+            this.Remetente.TabIndex = 1;
+            this.Remetente.Text = "Dados do envio";
+            // 
+            // TextBoxRequisicoes
+            // 
+            this.TextBoxRequisicoes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
+            this.TextBoxRequisicoes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxRequisicoes.ForeColor = System.Drawing.Color.White;
+            this.TextBoxRequisicoes.Location = new System.Drawing.Point(462, 86);
+            this.TextBoxRequisicoes.Multiline = true;
+            this.TextBoxRequisicoes.Name = "TextBoxRequisicoes";
+            this.TextBoxRequisicoes.ReadOnly = true;
+            this.TextBoxRequisicoes.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBoxRequisicoes.Size = new System.Drawing.Size(452, 551);
+            this.TextBoxRequisicoes.TabIndex = 4;
+            this.TextBoxRequisicoes.TextChanged += new System.EventHandler(this.TextBoxRequisicoes_TextChanged);
+            // 
+            // LabelRequisicoes
+            // 
+            this.LabelRequisicoes.AutoSize = true;
+            this.LabelRequisicoes.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelRequisicoes.ForeColor = System.Drawing.Color.White;
+            this.LabelRequisicoes.Location = new System.Drawing.Point(546, 48);
+            this.LabelRequisicoes.Name = "LabelRequisicoes";
+            this.LabelRequisicoes.Size = new System.Drawing.Size(276, 29);
+            this.LabelRequisicoes.TabIndex = 5;
+            this.LabelRequisicoes.Text = "Requisições protocolo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(463, 640);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Snd: Sender";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(807, 638);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Rcv: Receiver";
+            // 
+            // email
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(943, 681);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LabelRequisicoes);
+            this.Controls.Add(this.TextBoxRequisicoes);
+            this.Controls.Add(this.Remetente);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "email";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "E-mail";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label Remetente;
+        private System.Windows.Forms.TextBox TextBoxRequisicoes;
+        private System.Windows.Forms.Label LabelRequisicoes;
+        private System.Windows.Forms.Label LabelEmailRem;
+        private System.Windows.Forms.TextBox TextBoxAssunto;
+        private System.Windows.Forms.Label LabelAssunto;
+        private System.Windows.Forms.TextBox TextBoxEmailRem;
+        private System.Windows.Forms.Button BotaoEnviar;
+        private System.Windows.Forms.Label LabelMensagem;
+        private System.Windows.Forms.TextBox TextBoxMensagem;
+        private System.Windows.Forms.TextBox TextBoxEmailDest;
+        private System.Windows.Forms.Label LabelEmailDest;
+        private System.Windows.Forms.TextBox TextBoxSenha;
+        private System.Windows.Forms.Label LabelSenha;
+        private System.Windows.Forms.CheckBox checkBoxEnviarEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ProtocoloEmail/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ProtocoloEmail
+{
+    public partial class email : Form
+    {
+        int FQuantidadeEnvios = 0;
+
+        public email()
+        {
+            InitializeComponent();
+        }
+
+        private bool CheckCamposObrigatorios()
+        {
+            if ((string.IsNullOrEmpty(TextBoxEmailRem.Text)) || 
+                (string.IsNullOrEmpty(TextBoxSenha.Text)) ||
+                (string.IsNullOrEmpty(TextBoxEmailDest.Text)) ||
+                (string.IsNullOrEmpty(TextBoxAssunto.Text)) ||
+                (string.IsNullOrEmpty(TextBoxMensagem.Text)))
+            {
+                return false;
+            } 
+            else
+            {
+                return true;
+            }    
+        }
+
+        private bool VerificarServidorEmail(string prEmail)
+        {
+            //ATRIBUI A EXPRESSAO REGEX
+            Regex regExpEmailServer = new Regex(@"@[a-z]+\.[a-z]{2,3}");
+
+            //VALIDA A EXPRESSAO
+            Match match = regExpEmailServer.Match(prEmail);
+
+            if (match.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool VerificarEmail(string prEmail)
+        {
+            //ATRIBUI A EXPRESSAO REGEX
+            Regex regExpEmail = new Regex(@"^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*\s+<(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})>$|^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$");
+
+            //VALIDA A EXPRESSAO
+            Match match = regExpEmail.Match(prEmail);
+
+            if (match.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private string FormatarData(DateTime prData)
+        {
+            string vFormato       = "r";
+            string vDataFormatada = "";
+
+            for (int i = 0; i < vFormato.Length; i++)
+            {
+                vDataFormatada = prData.ToString(vFormato, DateTimeFormatInfo.InvariantInfo);
+                Console.WriteLine(string.Concat(vFormato, " :", vDataFormatada));
+            }
+
+            return vDataFormatada;
+        }
+
+        private void BotaoEnviar_Click(object sender, EventArgs e)
+        {
+            //ALIMENTANDO VARIAVEIS COM OS CAMPOS DO TEXT BOX
+            string vEmailDestinatario = TextBoxEmailDest.Text;
+            string vEmailRemetente = TextBoxEmailRem.Text;
+            string vMensagem = TextBoxMensagem.Text;
+            string vProvedor = "";
+            string vAssunto = TextBoxAssunto.Text;
+            string vSenha = TextBoxSenha.Text;
+
+            //CHECA O PRENCHIMENTO DOS CAMPOS
+            if (!CheckCamposObrigatorios())
+            {
+                MessageBox.Show("Campos obrigatórios não prenchidos, para enviar a mensagem todos os campos devem ser preenchidos");
+                return;
+            }
+
+            //GAMBIZINHA PARA MOSTRA O NUMERO DA REQUISIÇÃO
+            FQuantidadeEnvios = FQuantidadeEnvios + 1;
+
+            try
+            {
+                //BUSCA O SERVIDOR DE EMAIL DO REMETENTE
+                string[] vServidor = vEmailRemetente.Split('@');
+                vProvedor = vServidor[1];
+            }
+            catch
+            {
+
+            }
+
+            TextBoxRequisicoes.Text += "\r\n============ REQUISIÇÃO NR " + FQuantidadeEnvios + " ============\r\n";
+
+            TextBoxRequisicoes.Text += "Snd: HELO " + vProvedor + "\r\n";
+
+            if (!VerificarServidorEmail(vEmailRemetente))
+            {
+                TextBoxRequisicoes.Text += "Rcv: 512 email domain in the CC is incorrect\r\n";
+
+                MessageBox.Show("Ocorreu um erro na requisição SMTP");
+                return;
+            }
+            else
+            {
+                TextBoxRequisicoes.Text += "Rcv: 250 Hello " + vProvedor + ", pleased to meet you\r\n";
+            }
+
+            //EMAIL REMETENTE
+            TextBoxRequisicoes.Text += "Snd: MAIL FROM: <" + vEmailRemetente + ">\r\n";
+
+            if (!VerificarEmail(vEmailRemetente))
+            {
+                TextBoxRequisicoes.Text += "Rcv: 510 email address in the 'CC' is incorrect\r\n";
+
+                MessageBox.Show("Ocorreu um erro na requisição SMTP");
+                return;
+            }
+            else
+            {
+                TextBoxRequisicoes.Text += "Rcv: 250 OK!\r\n";
+            }
+
+            //EMAIL DESTINATARIO
+            TextBoxRequisicoes.Text += "Snd: RCPT TO: <" + vEmailDestinatario + ">\r\n";
+
+            if (!VerificarEmail(vEmailDestinatario))
+            {
+                TextBoxRequisicoes.Text += "Rcv: 510 email address in the 'TO' is incorrect\r\n";
+
+                MessageBox.Show("Ocorreu um erro na requisição SMTP");
+                return;
+            }
+            else
+            {
+                TextBoxRequisicoes.Text += "Rcv: 250 OK!\r\n";
+            }
+
+            //DADOS DO EMAIL
+            TextBoxRequisicoes.Text += "Snd: DATA\r\n";
+
+            TextBoxRequisicoes.Text += "Rcv: 353 End data with <CR><LF> <CR><LF>\r\n";
+
+            TextBoxRequisicoes.Text += "Snd: From: " + vEmailRemetente + "\r\n";
+            TextBoxRequisicoes.Text += "Snd: To: " + vEmailDestinatario + "\r\n";
+
+            string vDataEnvio = FormatarData(DateTime.Now);
+
+            TextBoxRequisicoes.Text += "Snd: Date: " + vDataEnvio + "\r\n";
+
+            TextBoxRequisicoes.Text += "Snd: Subject: " + vAssunto + "\r\n";
+            TextBoxRequisicoes.Text += "Snd: \r\n";
+
+            TextBoxRequisicoes.Text += "Snd: " + vMensagem + "\r\n";
+
+            Random vNumeroAleatorio = new Random();
+            int vNumeroFila = vNumeroAleatorio.Next(1, 10000);
+
+            TextBoxRequisicoes.Text += "Rcv: 250 Ok: queued at " + vNumeroFila + "\r\n";
+
+            TextBoxRequisicoes.Text += "Snd: QUITS\r\n";
+
+            TextBoxRequisicoes.Text += "Rcv: 221 Bye\r\n";
+            TextBoxRequisicoes.Text += "{The server closes the connection}";
+
+            bool vEhEnviarEmail = checkBoxEnviarEmail.Checked;
+
+            if (vEhEnviarEmail)
+            {
+                //MONTAGEM DO EMAIL
+                MailMessage mail = new MailMessage();
+
+                mail.From = new MailAddress(vEmailRemetente);
+                mail.Subject = vAssunto;
+                mail.Body = vMensagem;
+
+                mail.To.Add(vEmailDestinatario);
+
+                using (var smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.Credentials = new NetworkCredential(vEmailRemetente, vSenha);
+
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+
+                    //ENVIO DO EMAIL
+                    try
+                    {
+                        smtp.Send(mail);
+
+                        MessageBox.Show("E-mail Enviado com sucesso!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Falha ao enviar e-mail");
+                    }
+                }
+            }
+        }
+
+        private void TextBoxRequisicoes_TextChanged(object sender, EventArgs e)
+        {
+            TextBoxRequisicoes.SelectionStart = TextBoxRequisicoes.Text.Length;
+
+            TextBoxRequisicoes.ScrollToCaret();
+        }
+    }
+}A
