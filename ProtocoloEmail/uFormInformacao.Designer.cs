7fc10dbf6ac9ecc4bdbad1770f5b338652507e4b
Evandro Rode Junior <evandrorodejunior@gmail.com>
2022-03-24 13:10:14 -0300

    Instructions for use included

--- ProtocoloEmail/uFormInformacao.Designer.cs
+++ ProtocoloEmail/uFormInformacao.Designer.cs
@@ -29,23 +29,41 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(uFormInformacao));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.groupBox1.Location = new System.Drawing.Point(12, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(417, 640);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // uFormInformacao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(441, 450);
+            this.ClientSize = new System.Drawing.Size(445, 642);
+            this.Controls.Add(this.groupBox1);
             this.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "uFormInformacao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Informações de uso";
+            this.Load += new System.EventHandler(this.uFormInformacao_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- ProtocoloEmail/uFormInformacao.cs
+++ ProtocoloEmail/uFormInformacao.cs
@@ -16,5 +16,26 @@ namespace ProtocoloEmail
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void uFormInformacao_Load(object sender, EventArgs e)
+        {
+            groupBox1.Text = "\n " +
+                             "Cada e-mail tem o endereço do remetente (por exemplo, remetente@email.com.br) e sua senha de login (por exemplo, !@#ROOT). \n" +
+                             "E para o destinatário(por exemplo, destinatario @email.com.br). \n" +
+                             "Onde caso o endereço do remetente e do destinatário, quando um e-mail é enviado, o provedor de e-mail se conecta ao servidor SMTP do serviço de e-mail do remetente. \n" +
+                             "O provedor transmite o endereço do remetente, o endereço do destinatário e o conteúdo da mensagem. \n" +
+                             "Assim, o servidor SMTP trabalha para localizar o paradeiro do destinatário.Usando o ID de e-mail do destinatário, ele localiza o nome do domínio.\n\n" +
+                             "Nesse “simulador” é possível também enviar emails a um destinatário real.\n" +
+                             "Basta marcar a opção de enviar e-mail de forma real ao destinatário.\n" +
+                             "Caso ao tentar enviar o email de forma real e o servidor seja o Gmail, e aconteça o erro em que o servidor SMTP requer uma conexão segura ou o cliente não foi autenticado.\n" +
+                             "Nas configurações da conta:\n" +
+                             "Procure pela opção ''Acesso menos seguro'', ou entre aqui: https://myaccount.google.com/lesssecureapps \n" +
+                             "Ativa a opção de 'Acesso menos seguro' (isso só será possível se sua conta não possuir autenticação por 2 fatores)\n";
+        }
     }
 }
